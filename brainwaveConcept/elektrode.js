83da086652af012926fb183944cbed8e9d08bc68
luuuucaaa <60258241+luuuucaaa@users.noreply.github.com>
2020-04-28 13:58:53 +0200

    added sliders, bug fixes

--- brainwaveConcept/elektrode.js
+++ brainwaveConcept/elektrode.js
@@ -9,6 +9,33 @@ class Elektrode{
     this.freq = freq;
   }
 
+  slider() {
+    this.sliderAlpha = createSlider(0, 255, random(0, 255));
+    this.sliderAlpha.position(this.posX, this.posY + 40);
+    this.sliderAlpha.style('width', '60px');
+
+    this.sliderLoBeta = createSlider(0, 255, random(0, 255));
+    this.sliderLoBeta.position(this.posX, this.posY + 40 + 20);
+    this.sliderLoBeta.style('width', '60px');
+
+    this.sliderHiBeta = createSlider(0, 255, random(0, 255));
+    this.sliderHiBeta.position(this.posX, this.posY + 40 + 40);
+    this.sliderHiBeta.style('width', '60px');
+
+    this.sliderGamma = createSlider(0, 255, random(0, 255));
+    this.sliderGamma.position(this.posX, this.posY + 40 + 60);
+    this.sliderGamma.style('width', '60px');
+  }
+
+  sliderText() {
+    stroke(255);
+    strokeWeight(.8);
+    text('Alpha', this.sliderAlpha.x - 60, this.sliderAlpha.y + 13);
+    text('LoBeta', this.sliderLoBeta.x - 60, this.sliderLoBeta.y + 13);
+    text('HiBeta', this.sliderHiBeta.x - 60, this.sliderHiBeta.y + 13);
+    text('Gamma', this.sliderGamma.x - 60, this.sliderGamma.y + 13);
+  }
+
   display() {
     this.alpha = 255 - 0.5 * dist(this.posX, this.posY, mouseX, mouseY);
     noFill();
@@ -18,14 +45,24 @@ class Elektrode{
   }
 
   startSound() {
-    this.sound = new p5.Oscillator('triangle');
-    this.sound.freq(this.freq);
-    this.sound.amp(0.1);
-    this.sound.start();
-  }
+    this.soundAlpha = new p5.Oscillator('sine');
+    this.soundAlpha.freq(this.freq);
+    this.soundAlpha.amp(0.1);
+    this.soundAlpha.start();
 
-  // modSound() {
-  //   let modAmp = map(dist(e1.posX, e1.posY, mouseX, mouseY), 0, width, 0.08, 0);
-  //   this.sound.amp(modAmp, 0.1);
-  // }
+    this.soundLoBeta = new p5.Oscillator('sine');
+    this.soundLoBeta.freq(2 * this.freq);
+    this.soundLoBeta.amp(0.1);
+    this.soundLoBeta.start();
+
+    this.soundHiBeta = new p5.Oscillator('sine');
+    this.soundHiBeta.freq(3 * this.freq);
+    this.soundHiBeta.amp(0.1);
+    this.soundHiBeta.start();
+
+    this.soundGamma = new p5.Oscillator('sine');
+    this.soundGamma.freq(4 * this.freq);
+    this.soundGamma.amp(0.1);
+    this.soundGamma.start();
+  }
 }
--- brainwaveConcept/sketch.js
+++ brainwaveConcept/sketch.js
@@ -15,25 +15,33 @@ let colors = [
                 [0, 255, 255],
                 [255, 0, 255]
               ];
-let freqs = [1220, 1223, 880, 874, 438, 442];
+let freqs = [1220, 0, 0, 0, 0, 0];
 
 let elektroden = [];
 let diameter = 20;
 
 function setup() {
-  fullscreen(true);
   createCanvas(1280, 720);
   for (let i = 0; i < number; i++) {
     elektroden[i] = new Elektrode(width * coordinates[i][0], height * coordinates[i][1], diameter, colors[i][0], colors[i][1], colors[i][2], freqs[i]);
     elektroden[i].startSound();
+    elektroden[i].slider();
   }
 }
 
 function draw() {
   background(0);
+
   for (let i = 0; i < number; i++) {
+    elektroden[i].sliderText();
     elektroden[i].display();
-    let modAmp = map(dist(elektroden[i].posX, elektroden[i].posY, mouseX, mouseY), 0, width, 0.008, 0);
-    elektroden[i].sound.amp(modAmp, 0.1);
+
+    let inverseDistance = width/2 - dist(elektroden[i].posX, elektroden[i].posY, mouseX, mouseY);
+    if (inverseDistance < 0) {inverseDistance = 0}
+    let modAmp = map(inverseDistance, 0, width/2, 0, 0.1);
+    elektroden[i].soundAlpha.amp(modAmp, 0.1);
+    elektroden[i].soundLoBeta.amp(modAmp, 0.1);
+    elektroden[i].soundHiBeta.amp(modAmp, 0.1);
+    elektroden[i].soundGamma.amp(modAmp, 0.1);
   }
 }
